228b9a88ccfa51be40598a79bd1248ece052df4d
storiaca <storiaca@gmail.com>
2018-11-03 13:46:45 +0100

    Create list component

---
+++ src/components/List.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+export default class List extends Component {
+  render() {
+    const { items, itemElement: Item } = this.props;
+    return (
+      <div>
+        {items.map(item => (
+          <Item {...item} key={item.id} />
+        ))}
+      </div>
+    );
+  }
+}
